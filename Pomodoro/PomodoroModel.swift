1bd6a645ed60acf06d3d8a478793abbf1be3e756
Eugene Maltsev <no.one.left.disappointed@gmail.com>
2021-04-18 09:11:15 +0300

    R.K temporary extensions

--- Pomodoro/PomodoroModel.swift
+++ Pomodoro/PomodoroModel.swift
@@ -11,6 +11,27 @@ protocol PomodoroModelDelegate: class {
     func didRecieveTimeUpdate(times: String)
 }
 
+func a1 () -> Bool {
+    
+    let a = 1
+    let b = 2
+    
+    func df () -> Bool {
+        return a == b
+    }
+    
+    let df2 = {
+        a == b
+    }
+    
+    return df2()
+}
+
+var someInt = 0
+
+func regularTimerFunc(timer: Timer) {
+    
+}
 
 class PomodoroModel {
     
@@ -21,12 +42,68 @@ class PomodoroModel {
     private var countBreak = 250
     private var work = false
 // 1500 300
+    
+    class MyTimerFunc {
+        let cycleRef: PomodoroModel
+        init(cycleRef: PomodoroModel) {
+            self.cycleRef = cycleRef
+        }
+    }
+    class MyTimer {
+        let myFunc: MyTimerFunc
+        init(myFunc: MyTimerFunc) {
+            self.myFunc = myFunc
+        }
+    }
+    var myTimer: MyTimer?
 
+    func compareStrings(_ lhs: String, _ rhs: String) -> Bool {
+        return lhs < rhs
+    }
+    
     func startTimer() {
-//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
-//            (timer: Timer) -> Void in
-//            self.countTimer()
-//        })
+        
+        self.myTimer = MyTimer(myFunc: MyTimerFunc(cycleRef: self))
+        
+        struct MyClosure {
+            weak var self1: PomodoroModel?
+            // var self2: PomodoroModel
+            
+            func onTimer(timer: Timer) {
+                guard let self2 = self1 else { return }
+                assert(self2.timer == timer)
+                self2.countTimer()
+            }
+        }
+        
+        self.myTimer = nil
+        
+        let myClosure = MyClosure(self1: self)
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1,
+                                          repeats: true,
+                                          block:myClosure.onTimer)
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+            [weak weakSelf = self] (timer: Timer) -> Void in
+            guard let strongSelf = weakSelf else { return }
+            assert(strongSelf.timer == timer)
+            strongSelf.countTimer()
+        })
+        
+        self.timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {
+            [weak self] (timer: Timer) -> Void in
+            guard let self = self else { return }
+            assert(self.timer == timer)
+            self.countTimer()
+        })
+        
+        let arr: [String] = ["a", "b", "c"]
+        let _ = arr.sorted() {
+            self.compareStrings($0, $1)
+        }
+        
+        
+        
         
 //        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
 //            (timer: Timer) -> Void in
@@ -37,11 +114,11 @@ class PomodoroModel {
 //            (timer) in
 //            self.countTimer()
 //        }
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
-            [weak self] timer in
-            self?.countTimer()
-        }
+                
+//        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {
+//            [weak self] timer in
+//            self?.countTimer()
+//        }
     }
     
     func countTimer() {
@@ -75,6 +152,8 @@ class PomodoroModel {
     }
     
     func reset() {
+        
+        
         timer?.invalidate()
         timer = nil
         count = 1500
